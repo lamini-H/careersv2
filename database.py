cb5a7e98af50a7e6e339b5a387ca18bccc6e7c09
Harunah <113555226+lamini-H@users.noreply.github.com>
2023-03-10 07:47:01 +0000

    Database engine created using sqlalchemy and pymysql

---
+++ database.py
@@ -0,0 +1,3 @@
+from  sqlalchemy import create_engine
+
+engine = create_engine("mysql+pymysql://darze6ylcmfbn0ed7zj2:pscale_pw_cc70S7xieHOVesMxuOYJfnVHsOK6QAlkkDKfOUsxHCO@us-east.connect.psdb.cloud/lamscareers?charset=utf8mb4")
