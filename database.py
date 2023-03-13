b1024c176a6ac8cb8fed69c520262077451f1d64
Harunah <113555226+lamini-H@users.noreply.github.com>
2023-03-13 08:34:22 +0000

    Made changes to the load_jobs_from_db functions to return all records

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
-from  sqlalchemy import create_engine, text
+from sqlalchemy import create_engine, text
+
 
 db_connection_string ="mysql+pymysql://darze6ylcmfbn0ed7zj2:pscale_pw_cc70S7xieHOVesMxuOYJfnVHsOK6QAlkkDKfOUsxHCO@us-east.connect.psdb.cloud/lamscareers?charset=utf8mb4"
 
@@ -14,16 +15,16 @@ engine = create_engine(
 def load_jobs_from_db():
     with engine.connect() as conn:
     # Execute a SQL query to select all rows from the "jobs" table
-      query = text('select title,location,salary from jobs')
+      query = text('select * from jobs')
       result = conn.execute(query)
 
     # Iterate through the rows and convert them to dictionaries
-    jobs = []
+    job = []
     for row in result.fetchall():
         result_dict = {}
         for index, column in enumerate(result.keys()):
             result_dict[column] = row[index]
-        jobs.append(result_dict)
-        return jobs
-
-      
+        job.append(result_dict)
+    return job
+    
+   
